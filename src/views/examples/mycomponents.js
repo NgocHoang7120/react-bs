f76caa61e2a3f649667e3b9ada1e3e8bf0bcb15b
HoangNgoc0701 <yolov6colab@gmail.com>
2022-11-25 10:06:31 +0700

    state

--- src/views/examples/mycomponents.js
+++ src/views/examples/mycomponents.js
@@ -4,11 +4,27 @@ import React from 'react';
 class MyComponent extends React.Component {
     //Dùng hàm render để render dữ liệu
     // trả ra cái gì thì return trong render
+// cú pháp jsx return 1 khối code
+// jsx cho phép viết cú pháp javascript trong html
+// khai báo state global
+    state = {
+        name: 'CNH',
+        age: 21,
+        address: 'Ha noi'
+    }
+
     render() {
+        let name = 'NTH';
         return(
-            <div>
-                hello my component from VsCode
-            </div>
+            <React.Fragment> 
+                <div className='abc'>
+                    hello my component from VsCode, My name is {name}
+                </div>
+                <div className='123'>
+                    okeeee {this.state.address}
+                </div>
+            </React.Fragment>
+
         )
     }
 };
