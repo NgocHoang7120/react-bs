be2bd3f24896b3abffab66e64b5aac3a1611ed5d
HoangNgoc0701 <yolov6colab@gmail.com>
2022-12-07 20:30:24 +0700

    withRouter

--- src/views/examples/home.js
+++ src/views/examples/home.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import { withRouter } from "react-router";
 class Home extends React.Component {
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.history.push('/Todos')
+        }, 5000);
+    }
     render() {
+        console.log(">>>check props: ", this.props);
         return(
             <>
                 <div>
@@ -12,4 +18,4 @@ class Home extends React.Component {
     }
 };
 
-export default Home;
+export default withRouter(Home);
